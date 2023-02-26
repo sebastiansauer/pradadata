1946204149d3f71facd5ccfc25fc09a47ea363b2
Sebastian Sauer <ssauer@posteo.de>
2023-02-26 20:42:00 +0100

    fix data cols

--- R/OBECT_corr_data.R
+++ R/OBECT_corr_data.R
@@ -10,12 +10,13 @@
 #' each: #' -0.999999, -0.90, -0.75, -0.30, 0.00, 0.30, 0.75, 0.90, 0.999999. 
 #' Each value has a different id.
 #'
-#' @format A data frame containing 900 rows and 6 columns
+#' @format A data frame containing 900 rows and 5 columns
 #' \describe{
 #'   \item{V1}{Variable 1}
 #'   \item{V2}{Variable 2}
 #'   \item{correlationn}{correlation coefficient (Pearson)}
-#'   \item{id}{id of the correlation (1-9)}
+#'   \item{id_cor}{id of the correlation (1-9)}
+#'   \item{id}{line number}
 #' }
 #'
 
